3e11895749cc3792ffbad6e5719bc28a948781fe
Sheel Shah <161749196+Sheel9305@users.noreply.github.com>
2025-04-07 12:16:15 +0530

    server.js

--- server.js
+++ server.js
@@ -1,21 +1,15 @@
-// Import required modules
 const express = require('express');
 const cors = require('cors');
 
-// Create an instance of the Express application
 const app = express();
 const port = 3000;
 
-// Middleware to allow CORS
 app.use(cors());
 
-// Sample ECG data (this should be populated with actual data from your ESP32)
 let ecgData = [];
 
-// Endpoint to receive ECG data from ESP32
 app.post('/ecg-data', (req, res) => {
     req.on('data', chunk => {
-        // Append incoming data to ecgData
         ecgData.push(JSON.parse(chunk));
     });
     req.on('end', () => {
@@ -23,12 +17,10 @@ app.post('/ecg-data', (req, res) => {
     });
 });
 
-// Endpoint to send ECG data
 app.get('/ecg-data', (req, res) => {
-    res.json(ecgData); // Send ECG data as JSON
+    res.json(ecgData);
 });
 
-// Start the server
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
